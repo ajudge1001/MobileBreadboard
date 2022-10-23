2d271ac82bfe27336bf379f4f5fb9ba1313f55aa
Alex S Judge <irontakeout@gmail.com>
2022-10-23 02:41:02 +0100

    Initial Commit

---
+++ MobileBreadboard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MobileBreadboard
+//
+//  Created by Alex Judge on 10/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("Road")
+            .resizable()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MobileBreadboard/MobileBreadboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileBreadboardApp.swift
+//  MobileBreadboard
+//
+//  Created by Alex Judge on 10/23/22.
+//
+
+import SwiftUI
+
+@main
+struct MobileBreadboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
