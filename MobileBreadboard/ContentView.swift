114eb5e4b8a18b584423aacb35d500cc14369c4f
Alex S Judge <irontakeout@gmail.com>
2022-10-23 04:06:33 +0100

    messing around

--- MobileBreadboard/ContentView.swift
+++ MobileBreadboard/ContentView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image("Road")
-            .resizable()
+        ScrollView {
+            Image("Road")
+                .resizable()
+                .scaledToFit()
+            
+            Text("The Road")
+                .font(.largeTitle)
+                .bold()
+                .multilineTextAlignment(.center)
+            
+            Text("Galway, Ireland")
+                .font(.title)
+                .foregroundColor(.secondary)
+            
+            Text(" \"Why do I smell\" ")
+            
+            Text("Wonderful session. Thank you very much for putting this tutorial together. It was awesome. I am a newbie and learned quite a lot from this tutorial. I will check out the other tutorials that you mentioned. God bless you!")
+                .font(.title3)
+                .bold()
+                .padding(.top)
+            Text("If you've watched this and decided you want to continue learning SwiftUI, I have great news: I have a 100% free series of tutorials that teach you everything you need to know, including lots of videos, interactive tests, challenges, and more. It's called the 100 Days of SwiftUI, and you can find it here: ")
+        }
+        .navigationTitle("Hello :)")
     }
 }
 
--- MobileBreadboard/MobileBreadboardApp.swift
+++ MobileBreadboard/MobileBreadboardApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct MobileBreadboardApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView{
+                ContentView()
+            }
         }
     }
 }
